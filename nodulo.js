10b588609e7449955a83aa4bc4273479f9d43008
vkbr <2266@kubrusly.com>
2022-08-28 01:58:50 -0300

    server using route a caminho da \'database\'.

--- nodulo.js
+++ nodulo.js
@@ -1,3 +1,7 @@
+const fs = require ('fs');
+const raw = fs.readFileSync('words.json');
+let words = JSON.parse(raw);
+console.log(words);
 
 const express = require('express');
 const app = express();
@@ -5,14 +9,24 @@ const port = 20853;
 //const p5 = require ("sketch");
 
 app.use(express.static('public'));
-app.get('/', (req, res) => {
- res.send('<h1>PORRRRRHAM</h1>')
 
+
+
+app.get('/escreve/:coisas/:num', (req, res) => {
+
+    var data = req.params;
+    var number = data.num;
+    var cs = data.coisas;
+    var reply = "";
+    for (var i = 0; i < number; i++) {
+        reply += "Essas " + cs + " " + number + "vezes. </br>";
+    }
+
+    res.send(reply);
 
 
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
-
