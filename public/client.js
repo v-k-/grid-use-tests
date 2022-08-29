ed9e3d734734f28c9af917679c19dfc904928b71
vkbr <2266@kubrusly.com>
2022-08-29 01:14:42 -0300

    inicio estudos p5 servido

---
+++ public/client.js
@@ -0,0 +1,30 @@
+let tela = new p5((p) => {
+  let now = 0.0;
+
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background(235, 178, 222);
+    now = p.millis();
+  }
+
+
+  p.draw = () =>{
+
+    //p.background(235, 178, 222);
+    p.fill(51,51);
+    p.noStroke();
+    
+    if(p.millis() - now > 2000){
+        p.ellipse(p.random(p.width), p.random(p.height), 60, 60);
+        now = p.millis();
+      }
+  }
+
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+    p.background(235, 178, 222);
+  }
+
+}, 'sketch_1');
